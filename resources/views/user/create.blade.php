a86d1c0d40af06f65de03634e719a583731de0bf
JasonMulya12 <mulyajason25@gmail.com>
2022-04-07 21:06:23 +0700

    Add Folder Fake_API

---
+++ resources/views/user/create.blade.php
@@ -0,0 +1,23 @@
+@extends ('layout.master')
+@section ('content')
+
+
+<div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('user.index') }}"> Back</a>
+        </div>
+
+<form method="post" action="{{ route('user.store')}}">
+    @csrf
+    <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">First Name</label>
+        <input type="text" name="nama_depan" class="form-control" id="exampleInputPassword1">
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Last Name</label>
+        <input type="text" name="nama_belakang" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+</form>
+@endsection
---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,53 @@
+@extends('layout.master')
+     
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Fake API</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="{{ route('user.create') }}"> Create</a>
+            </div>
+        </div>
+    </div>
+    
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+     
+    <table class="table table-bordered">
+        <tr>
+            <th>No</th>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th width="280px">Action</th>
+        </tr>
+    @php
+        $number = 1;
+    @endphp
+    
+        @forelse($users['data'] as $user)
+        <tr>
+        <td>{{ $number++ }}</td>
+        <td>{{ $user['firstName'] }}</td>
+        <td>{{ $user['lastName'] }}</td>
+        <td align="center">
+            <form method="POST" action="{{ 'user/'.$user['id'] }}">
+                @method('DELETE')
+                @csrf
+
+                <a href="{{ 'user/'.$user['id'] }}" class="text-primary"><i class="fa fa-fw fa-edit"></i> Edit</a> |
+                <button type="submit" class="text-danger btn btn-link" onClick="return confirm('Are you sure to delete this user?');"><i class="fa fa-fw fa-trash"></i> Delete</button>
+             </form>
+        </td>
+        </tr>
+        @empty
+        <tr><td colspan="6" align="center">No Record(s) Found!</td></tr>
+        @endforelse
+    </table>
+    
+        
+@endsection
